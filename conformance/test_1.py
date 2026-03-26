052d31c818786bd90f87ee867f698cf98b12d1fb
Ashrith Sagar <ashrith9sagar@gmail.com>
2026-03-26 12:03:57 +0530

    Updated submodules;
    Added KindVar Prototype;
    Currently, mypy tries to understand HKTs to some extent;

---
+++ conformance/test_1.py
@@ -0,0 +1,21 @@
+# tests/test_1.py
+
+from collections.abc import Iterable
+from typing import Callable, KindVar, TypeVar, reveal_type
+
+T = TypeVar("T")
+F = KindVar("F", bound=Iterable[T])
+A = TypeVar("A")
+B = TypeVar("B")
+
+
+def fmap(fn: Callable[[A], B], obj: F[A]) -> F[B]:
+    reveal_type(obj)  # F[A]
+    cls = type(obj)  # Should ideally be type[F], than type[F[A]], I think
+    reveal_type(cls)
+    tmp = (fn(a) for a in obj)
+    spec_cls = cls[B]  # Should be type[F[B]]
+    reveal_type(spec_cls)
+    res = spec_cls(tmp)
+    reveal_type(res)
+    return res
