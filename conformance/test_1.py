0e901d2fa7e8234d90a2ba4fa9b6226705bf7452
Ashrith Sagar <ashrith9sagar@gmail.com>
2026-03-27 01:45:14 +0530

    Updated submodules;
    KindVar resolution;

--- conformance/test_1.py
+++ conformance/test_1.py
@@ -9,7 +9,7 @@ A = TypeVar("A")
 B = TypeVar("B")
 
 
-def fmap(fn: Callable[[A], B], obj: F[A]) -> F[B]:
+def fmap1(fn: Callable[[A], B], obj: F[A]) -> F[B]:
     reveal_type(obj)  # F[A]
     cls = type(obj)  # Should ideally be type[F], than type[F[A]], I think
     reveal_type(cls)
@@ -17,5 +17,11 @@ def fmap(fn: Callable[[A], B], obj: F[A]) -> F[B]:
     spec_cls = cls[B]  # Should be type[F[B]]
     reveal_type(spec_cls)
     res = spec_cls(tmp)
+    reveal_type(res)
+    return res
+
+
+def fmap2(fn: Callable[[A], B], obj: F[A]) -> F[B]:
+    res = type(obj)(fn(a) for a in obj)
     reveal_type(res)
     return res
